f6d6b673b4f0f18cf593d35e0214e8e822a99328
Matthew Abruzzo <mwa2113@columbia.edu>
2018-01-29 16:54:46 -0500

    started to update analysis collection

--- lensanalysis/misc/analysis_collection.py
+++ lensanalysis/misc/analysis_collection.py
@@ -1,19 +1,11 @@
 from enum_definitions import Descriptor
 
-class AnalysisProductCollection(object):
-    """
-    Keeps track of the analysis products.
-    """
-
-    def __init__(self):
-        self.conv_map = None
-        self.tomo_conv_map = None
-        self.shear_map = None
-        self.tomo_shear_map = None
-        self.feature_products = None
-
-
 class ConvergenceMapProductCollection(object):
+    mapping = [(Descriptor.none, "noiseless_map"),
+               (Descriptor.noisy, "noisy_map"),
+               (Descriptor.smoothed, "smoothed_map"),
+               (Descriptor.smoothed_noisy, "smoothed_noisy_map")]
+
     def __init__(self):
         self.noiseless_map = None
         self.noisy_map = None
@@ -21,23 +13,59 @@ class ConvergenceMapProductCollection(object):
         self.smoothed_noisy_map = None
 
 class ShearMapProductCollection(object):
+    mapping = [(Descriptor.none, "noiseless_map"),
+               (Descriptor.noisy, "noisy_map")]
+
     def __init__(self):
         self.noiseless_map = None
         self.noisy_map = None
-
 
 class FeatureProductCollection(object):
     """
     If we add more features, this will be extended.
     """
+
+    mapping = [(Descriptor.none, "peak_loc", "peak_locations"),
+               (Descriptor.tomo, "peak_loc", "tomo_peak_locations"),
+               (Descriptor.none, "peak_counts", "peak_counts"),
+               (Descriptor.tomo, "peak_counts", "tomo_peak_counts")]
+    
     def __init__(self):
         self.peak_locations = None
         self.peak_counts = None
         self.tomo_peak_locations = None
         self.tomo_peak_counts = None
 
+class AnalysisProductCollection(object):
+    """
+    Keeps track of the analysis products.
+    """
+    conv_map_sub_collection = ConvergenceMapProductCollection
+    shear_map_sub_collection = ShearMapProductCollection
+    feature_product_sub_collection = FeatureProductCollection
+    
+    def __init__(self):
+        self.conv_map = None
+        self.tomo_conv_map = None
+        self.shear_map = None
+        self.tomo_shear_map = None
+        self.feature_products = None
 
+    @classmethod
+    def factory(cls):
+        out = cls()
+        collection_class = cls.conv_map_sub_collection
+        out.conv_map = collection_class()
+        out.tomo_conv_map = collection_class()
 
+        collection_class = cls.shear_map_sub_collection
+        out.shear_map = collection_class()
+        out.tomo_shear_map = collection_class()
+
+        collection_class = cls.feature_product_sub_collection
+        out.feature_products = collection_class()
+        return out
+        
 def _get_attribute_tuple(descriptors, object_name):
     if object_name == "conv_map":
         if Descriptor.tomo in descriptors:
