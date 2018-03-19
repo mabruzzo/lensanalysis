fixed bugs affecting the locating of peaks in convergence maps. The first bug prevented the highest value peak from being found (when no standard deviation is used for any maps or a single standard deviation is used for all maps of a tomographic bin). The second bug prevented cause the thresholds used for locating peaks using the standard deviation of each individual map to be completely wrong.

lensanalysis/procedure/peak_counting.py
@@ -26,7 +26,12 @@ class LocatePeaks(ConversionProcedureStep):
                             "{:d}").format(packet.data_id))
         out = []
         for elem in data_object:
-            extent = [np.nanmin(elem.data), np.nanmax(elem.data)]
+            #extent = [np.nanmin(elem.data), np.nextafter(np.nanmax(elem.data),
+            #                                             np.inf)]
+            # if norm is self.norm is True, then I am fairly confident that 
+            # during the c backend call, the extent values that we use are not 
+            # mulitiplied by the standard deviation.
+            extent = [-np.inf,np.inf]
             heights,positions = elem.locatePeaks(extent,norm=self.norm)
             out.append(PeakLocations(heights = heights, locations = positions))
         return out